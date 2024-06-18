11ac9b53b3260e4c71acdca03b4b8180fc2a6da4
David Maas <d.maas+git@neurogears.org>
2024-06-18 00:41:13 -0500

    WIP: Release automation

---
+++ .github/workflows/compare-nuget-packages.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+import hashlib
+import os
+import sys
+
+from pathlib import Path
+from zipfile import ZipFile, ZipInfo
+
+import gha
+import nuget
+
+# Symbol packages will change even for changes that we don't care about because the deterministic hash embedded in the PDB
+# is affected by the MVID of a package's dependencies. We don't want to release a new package when the only things that
+# changed were external to the package, so we don't check them.
+CHECK_SYMBOL_PACKAGES = False
+
+if len(sys.argv) != 5:
+    gha.print_error('Usage: compare-nuget-packages.py <previous-dummy-packages-path> <next-dummy-packages-path> <release-packages-path> <release-manifest-path>')
+    sys.exit(1)
+else:
+    previous_packages_path = Path(sys.argv[1])
+    next_packages_path = Path(sys.argv[2])
+    release_packages_path = Path(sys.argv[3])
+    release_manifest_path = Path(sys.argv[4])
+
+if not previous_packages_path.exists():
+    gha.print_error(f"Previous packages path '{previous_packages_path}' doest not exist.")
+if not next_packages_path.exists():
+    gha.print_error(f"Next packages path '{next_packages_path}' doest not exist.")
+if not release_packages_path.exists():
+    gha.print_error(f"Release packages path '{previous_packages_path}' doest not exist.")
+if release_manifest_path.exists():
+    gha.print_error(f"Release manifest '{release_manifest_path}' already exists.")
+gha.fail_if_errors()
+
+def verbose_log(message: str):
+    gha.print_debug(message)
+
+def should_ignore(file: ZipInfo) -> bool:
+    # Ignore metadata files which change on every pack
+    if file.filename == '_rels/.rels':
+        return True
+    if file.filename.startswith('package/services/metadata/core-properties/') and file.filename.endswith('.psmdcp'):
+        return True
+    
+    # Don't care about explicit directories
+    if file.is_dir():
+        return True
+    
+    return False
+
+def nuget_packages_are_equivalent(a_path: Path, b_path: Path, is_snupkg: bool = False) -> bool:
+    verbose_log(f"Comparing '{a_path}' and '{b_path}'")
+
+    # One package exists and the other does not
+    if a_path.exists() != b_path.exists():
+        verbose_log(f"Not equivalent: Only one package actually exists")
+        return False
+    
+    # The package doesn't exist at all, assume mistake unless we're checking the optional symbol packages
+    if not a_path.exists():
+        if is_snupkg:
+            verbose_log("Equivalent: Neither package exists")
+            return True
+        raise FileNotFoundError(f"Neither package exists: '{a_path}' or '{b_path}'")
+    
+    # From this point on: Check everything and emit messages for debugging purposes
+    is_equvalent = True
+
+    # Check if corresponding symbol packages are equivalent
+    if CHECK_SYMBOL_PACKAGES and not is_snupkg:
+        if not nuget_packages_are_equivalent(a_path.with_suffix(".snupkg"), b_path.with_suffix(".snupkg"), True):
+            verbose_log("Not equivalent: Symbol packages are not equivalent")
+            is_equvalent = False
+        else:
+            verbose_log("Symbol packages are equivalent")
+
+    # Compare the contents of the packages
+    # NuGet package packing is unfortunately not fully deterministic so we cannot compare the packages directly
+    # https://github.com/NuGet/Home/issues/8601
+    with ZipFile(a_path, 'r') as a_zip, ZipFile(b_path, 'r') as b_zip:
+        b_infos = { }
+        for b_info in b_zip.infolist():
+            if should_ignore(b_info):
+                continue
+            assert b_info.filename not in b_infos
+            b_infos[b_info.filename] = b_info
+
+        for a_info in a_zip.infolist():
+            if should_ignore(a_info):
+                continue
+
+            b_info = b_infos.pop(a_info.filename, None)
+            if b_info is None:
+                verbose_log(f"Not equivalent: '{a_info.filename}' exists in '{a_path}' but not in '{b_path}'")
+                is_equvalent = False
+                continue
+
+            if a_info.CRC != b_info.CRC:
+                verbose_log(f"Not equivalent: CRCs of '{a_info.filename}' do not match between '{a_path}' and '{b_path}'")
+                is_equvalent = False
+                continue
+            
+            if a_info.file_size != b_info.file_size:
+                verbose_log(f"Not equivalent: File sizes of '{a_info.filename}' do not match between '{a_path}' and '{b_path}'")
+                is_equvalent = False
+                continue
+
+            a_hash = hashlib.file_digest(a_zip.open(a_info), 'sha256').hexdigest() # type: ignore
+            b_hash = hashlib.file_digest(b_zip.open(b_info), 'sha256').hexdigest() # type: ignore
+            if a_hash != b_hash:
+                verbose_log(f"Not equivalent: SHA256 hashes of '{a_info.filename}' do not match between '{a_path}' and '{b_path}'")
+                is_equvalent = False
+                continue
+
+        # Ensure every file in B was processed
+        if len(b_infos) > 0:
+            is_equvalent = False
+            verbose_log(f"Not equivalent: The following file(s) exist in '{a_path}' but not in '{b_path}'")
+            for filename in b_infos:
+                verbose_log(f"  '{filename}'")
+
+    return is_equvalent
+
+different_packages = []
+next_packages = set()
+for file in os.listdir(next_packages_path):
+    if not file.endswith(".nupkg"):
+        continue
+
+    # We don't tolerate build metadata here because the nuget_packages_are_equivalent call doesn't either
+    if not file.endswith(".99.99.99.nupkg"):
+        gha.print_error(f"Package '{file}' does not have a dummy version.")
+
+    package_name = nuget.get_package_name(file)
+    next_packages.add(package_name)
+
+    if not nuget_packages_are_equivalent(next_packages_path / file, previous_packages_path / file):
+        verbose_log(f"'{file}' differs")
+        different_packages.append(package_name)
+
+previous_packages = set()
+for file in os.listdir(previous_packages_path):
+    if file.endswith(".nupkg"):
+        previous_packages.add(nuget.get_package_name(file))
+
+release_packages = set()
+for file in os.listdir(release_packages_path):
+    if file.endswith(".nupkg"):
+        release_packages.add(nuget.get_package_name(file))
+
+print()
+different_packages.sort()
+if len(different_packages) == 0:
+    print("There are no packages with any changes.")
+else:
+    print("The following packages have changes:")
+    for package in different_packages:
+        print(f"  {package}")
+
+# Ensure the next dummy reference and release package sets contain the same packages
+def list_missing_peers(heading: str, packages: set[str]) -> bool:
+    if len(packages) == 0:
+        return False
+    
+    print()
+    print(heading)
+    for package in packages:
+        print(f"  {package}")
+    return True
+
+list_missing_peers("The following packages are new for this release:", next_packages - previous_packages)
+list_missing_peers("The following packages were removed during this release:", previous_packages - next_packages)
+
+if list_missing_peers("The following packages exist in the release package artifact, but not in the next dummy reference artifact:", release_packages - next_packages):
+    gha.print_error("Some packages exist in the release package artifact, but not in the next dummy reference artifact.")
+if list_missing_peers("The following packages exist in the next dummy reference artifact, but not in the release package artifact:", next_packages - release_packages):
+    gha.print_error("Some packages exist in the next dummy reference artifact, but not in the release package artifact.")
+
+with open(release_manifest_path, 'x') as manifest:
+    for package in different_packages:
+        manifest.write(f"{package}\n")
+
+gha.fail_if_errors()
---
+++ .github/workflows/filter-release-packages.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+from pathlib import Path
+
+import gha
+import nuget
+
+if len(sys.argv) != 3:
+    gha.print_error('Usage: filter-release-packages.py <release-manifest-path> <packages-path>')
+    sys.exit(1)
+else:
+    release_manifest_path = Path(sys.argv[1])
+    packages_path = Path(sys.argv[2])
+
+if not release_manifest_path.exists():
+    gha.print_error(f"Release manifest '{release_manifest_path}' doest not exist.")
+if not packages_path.exists():
+    gha.print_error(f"Packages path '{packages_path}' doest not exist.")
+gha.fail_if_errors()
+
+release_packages = set()
+with open(release_manifest_path, 'r') as release_manifest:
+    for line in release_manifest.readlines():
+        release_packages.add(line.strip())
+
+file_names = os.listdir(packages_path)
+file_names.sort()
+for file_name in file_names:
+    extension = Path(file_name).suffix.lower()
+    if extension != '.nupkg' and extension != '.snupkg':
+        continue
+
+    package_name = nuget.get_package_name(file_name)
+    if package_name in release_packages:
+        if extension != '.snupkg':
+            print(f"✅ '{package_name}'")
+        continue
+    
+    if extension != '.snupkg':
+        print(f"⬜ '{package_name}'")
+    os.unlink(packages_path / file_name)
